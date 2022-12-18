d7b0dec3cb3c78a65b62622e221371f989182936
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2022-12-18 15:40:36 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,15 +1,6 @@
 def solution(n):
-    answer = ''
-    while True :
-        if n // 3 == 0:
-            answer += str(n%3)
-            break
-        answer += str(n%3)
+    ans = ''
+    while n != 0 :
+        ans += str(n % 3)
         n = n // 3
-    n = str(int(answer))[::-1]
-    answer = 0
-    cnt = 1
-    for i in str(n) :
-        answer += int(i)*cnt
-        cnt *= 3
-    return answer
+    return int(ans,3)
