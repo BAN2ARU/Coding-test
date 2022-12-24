50515fc13f294ffb3afda17f557723094c1eb4c1
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2022-12-24 17:53:30 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -0,0 +1,7 @@
+def solution(my_string, num1, num2):
+    answer = list(my_string)
+    tmp1 = answer[num1]
+    tmp2 = answer[num2]
+    answer[num1] = tmp2
+    answer[num2] = tmp1
+    return ''.join(answer)
