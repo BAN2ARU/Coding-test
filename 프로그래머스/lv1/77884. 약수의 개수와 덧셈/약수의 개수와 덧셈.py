c15e09518309dbee51892b49d9df0f42d64ec504
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2022-12-26 20:10:44 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,7 +1,7 @@
 def solution(left, right):
     answer = 0
     for i in range(left, right+1) :
-        if (i ** 0.5) == int(i**0.5) :
+        if i**0.5 == int(i**0.5) :
             answer -= i
         else :
             answer += i
