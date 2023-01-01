e46f757953f99cd631834584ea25e3f0b8a6ceed
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-01-01 14:41:30 +0900

    [level 2] Title: 숫자의 표현, Time: 0.77 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = 0
-    for i in range(2, n//2+2) :
+    for i in range(2, n+1) :
         if i % 2 == 0 :
             if n % i == i/2 and (n//i - n%i + 1) > 0 :
                 answer += 1
