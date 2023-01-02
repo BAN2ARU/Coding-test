430d5353f231a8e8c9aba1be72e06d8ca45a5301
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-01-02 22:35:16 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,6 +1,6 @@
 def solution(n, m):
-    a = max(n,m)
-    b = min(n, m)
-    max_value = lambda a,b : b if not a%b else max_value(b, a%b)
-    m = max_value(a, b)
-    return [m, a*b//m]
+    a = min(n, m)
+    b = max(n, m)
+    eq1 = lambda a,b : b if a%b == 0 else eq1(b, a%b)
+    v1 = eq1(b, a)
+    return [v1, a*b//v1]
