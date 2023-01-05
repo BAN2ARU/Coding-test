bc3ee010148e9db4b7c3d91d7883f8eba105d0a5
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-01-05 19:37:39 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,12 +1,3 @@
 def solution(my_string):
-    my_string = my_string.split()
-    answer = int(my_string[0])
-    digits = my_string[2::2]
-    letters = my_string[1::2]
-    for a,b in zip(digits, letters) :
-        print(a,b)
-        if b == '+' :
-            answer += int(a)
-        else :
-            answer -= int(a)   
-    return answer
+    my_string = my_string.replace(' - ', ' + -')
+    return sum(map(int, (my_string.split('+'))))
