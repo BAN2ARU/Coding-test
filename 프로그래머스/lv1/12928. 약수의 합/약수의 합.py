86e9960b5033c04da9cedfcb097dfbe367b3f4c7
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-03-23 15:35:07 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -1,5 +1,9 @@
 def solution(n):
     answer = 0
+    if n == 0 :
+        return 0
+    elif n == 1 :
+        return 1
     for i in range(1, n//2+1) :
         if n % i == 0 :
             answer += i
