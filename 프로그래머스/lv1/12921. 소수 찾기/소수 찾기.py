ed8672ca635fbd239b6ceed0d292c4f67e1beb2d
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-04-05 16:42:58 +0900

    [level 1] Title: 소수 찾기, Time: 126.96 ms, Memory: 19.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    array = [True]*(n+1)
+
+    m = int(n ** 0.5)
+    for i in range(2, m+1):
+        if array[i] == True:
+            for j in range(i*i, n+1, i):
+                array[j] = False
+
+    x = [i for i in range(2, n+1) if array[i] == True]
+    answer = len(x)
+    return answer
+    
