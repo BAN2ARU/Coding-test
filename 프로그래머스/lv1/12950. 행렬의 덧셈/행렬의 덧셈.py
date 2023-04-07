8f59b259545b0c06e35157e9d9651da1f6b3cb34
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-04-07 13:34:56 +0900

    [level 1] Title: 행렬의 덧셈, Time: 15.38 ms, Memory: 22.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,3 +1,2 @@
 def solution(arr1, arr2):
-    answer = [[a+b for a,b in zip(a1, b1)] for a1, b1 in zip(arr1, arr2)]
-    return answer
+    return [[i+j for i,j in zip(a,b)]for a, b in zip(arr1, arr2)]
