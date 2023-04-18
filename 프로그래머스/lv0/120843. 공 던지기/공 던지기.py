fe38b2982b7d23f86c528ece6be0ac5bfd86d511
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-04-18 16:40:23 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -1,4 +1,4 @@
 def solution(numbers, k):
-    numbers = numbers*2
+    numbers = numbers * 2
     numbers = numbers[::2]
-    return numbers[(k-1)%len(numbers)]
+    return numbers[k%len(numbers)-1]
