a0259b99cbc9ec667cc3835b17d56c03dfbb8280
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-03 06:40:19 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
@@ -1,20 +1,4 @@
 def solution(score):
-    answer = []
-    temp = []
-    score = [sum(s)/2 for s in score]
-    scores = sorted(score,reverse=True)
-    ans_dict = dict()
-    tmp = 0
-    tmp_score = 0
-    for s in scores :
-        if temp[-1:] != [s] :
-            tmp += 1 + tmp_score
-            temp.append(s)
-            ans_dict[s] = tmp
-            tmp_score = 0
-        else :
-            tmp_score += 1
-            
-    for s in score :
-        answer.append(ans_dict[s])
-    return answer
+    score = [sum(s) for s in score]
+    s_score = sorted(score, reverse=True)
+    return [s_score.index(i)+1 for i in score]
