c1b5660286210d038c8b419bbffd39baec888079
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-04 16:40:55 +0900

    [level 1] Title: 모의고사, Time: 1.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,27 +1,21 @@
 def solution(answers):
-    answer = []
     l = len(answers)
-    one = [1, 2, 3, 4, 5] * ((l // 5) + 1)
-    two = [2, 1, 2, 3, 2, 4, 2, 5] * ((l // 8) + 1)
-    three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * ((l // 10) + 1)
-    a1, b2, c3 = 0, 0, 0
-    for i, num in enumerate(answers) :
-        if num == one[i] :
-            a1 += 1
-        if num == two[i] :
-            b2 += 1
-        if num == three[i] :
-            c3 += 1
-    ans = sorted([a1, b2, c3], reverse=True)
+    one = [1,2,3,4,5] * (l//5+1) 
+    two = [2,1,2,3,2,4,2,5] * (l//8+1) 
+    thr = [3,3,1,1,2,2,4,4,5,5] * (l//10+1)
+    score = [0,0,0]
+    for an, on, tw, th in zip(answers, one, two, thr) :
+        if an == on :
+            score[0] += 1
+        if an == tw :
+            score[1] += 1
+        if an == th :
+            score[2] += 1
+    m = max(score)
+    num = [1, 2, 3]
+    answer = list()
     
-    if ans.index(a1) == 0 :
-        answer.append(1)
-    if ans.index(b2) == 0 :
-        answer.append(2)
-    if ans.index(c3) == 0 :
-        answer.append(3)
-    
+    for i in range(len(score)):
+        if score[i] == m :
+            answer.append(num[i])
     return answer
-    
-    
-    
