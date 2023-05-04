406eb47153b24327329c0406550305d2d9f008d5
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-04 16:31:33 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,9 +1,7 @@
 def solution(k, m, score):
-    answer = 0
     score = sorted(score, reverse=True)
+    answer = 0
+    for i in range(m, len(score)+1, m) :
+        answer += min(score[i-m:i]) * m
     
-    num = len(score) // m
-    
-    for i in range(num) :
-        answer += min(score[i*m:(i+1)*m]) *m
     return answer
