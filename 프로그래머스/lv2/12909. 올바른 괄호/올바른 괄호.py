8e74cea2df15fac16cdd4a6facb6ca06774c5977
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-10 15:23:32 +0900

    [level 2] Title: 올바른 괄호, Time: 14.45 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,10 +1,13 @@
 def solution(s):
-    tmp = list(s[0])
-    for i in s[1:] :
+    answer = True
+    tmp = list()
+    
+    for i in s :
         if not tmp :
             tmp.append(i)
-        elif tmp[-1] + i == '()' :
+        elif tmp[-1]+i == '()':
             tmp.pop()
         else :
-            tmp.append(i)
-    return False if len(tmp) else True
+            tmp.append(i)        
+
+    return False if tmp else True
