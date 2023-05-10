73d0dbcef024b606c27f84f463c705ccf3275305
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-10 15:36:33 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,13 +1,11 @@
 def solution(a, b):
-    answer = 0
-    gcm = lambda x,y: y if x%y == 0 else gcm(y, x%y)
-    gcm_v = gcm(max(a,b), min(a,b))
-    b = b // gcm_v
-
-    
-    for i in range(2, b//2 + 1) :
-        if b % i == 0 :
-            if i % 2 == 0 or i % 5 == 0: 
-                continue
-            return 2
-    return 1 if b % 2 == 0 or b % 5 ==0 or b==1 else 2
+    eq = lambda x,y : y if x%y==0 else eq(y, x%y)
+    num = b // eq(a,b)
+    while True :
+        if num % 2 == 0 :
+            num = num//2
+        elif num % 5 == 0 :
+            num = num//5
+        else :
+            break
+    return 1 if num==1 else 2
