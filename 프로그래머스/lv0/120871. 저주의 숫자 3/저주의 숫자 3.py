46302023d0e7534a5348f296ace3c2c83af874f7
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-10 15:16:29 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,10 +1,12 @@
 def solution(n):
     answer = 0
-    for i in range(1, n+1) :
+    for _ in range(n) :
+        answer += 1
         while True :
-            answer += 1
-            if (answer%3 == 0) or ('3' in str(answer)) :
-                continue
+            if answer % 3 == 0 :
+                answer += 1
+            elif '3' in str(answer) :
+                answer += 1
             else :
                 break
     return answer
