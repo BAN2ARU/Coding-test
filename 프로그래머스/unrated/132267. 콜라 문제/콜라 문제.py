fb4a23d1c70c79d0929231a7882091eaad1f7b2f
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-11 13:18:02 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,8 +1,7 @@
 def solution(a, b, n):
     answer = 0
-    while True :
-        answer += (n//a)*b
-        n = (n//a)*b + n%a
-        if n < a :
-            break
+    while (n >= a) :
+        coke = n//a * b
+        answer += coke
+        n = coke + n%a
     return answer
