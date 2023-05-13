67239463dea0fe3f8d614f23512e216e94ea1620
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-13 20:58:42 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+lst = [i for i in range(1, n+1)]
+
+for _ in range(m) :
+    a, b = map(int, input().split())
+    tmp = lst[a-1:b]
+    lst[a-1:b] = tmp[::-1]
+print(*lst)
