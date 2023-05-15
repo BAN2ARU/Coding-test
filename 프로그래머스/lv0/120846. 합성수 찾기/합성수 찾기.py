b9ba184ce5be792982f2d3b22473d8d925e929f4
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-15 12:47:19 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,6 +1,7 @@
 def solution(n):
     answer = 0
-    for num in range(1, n+1) :
+    
+    for num in range(4, n+1) :
         for i in range(2, int(num**0.5)+1) :
             if num % i == 0 :
                 answer += 1
