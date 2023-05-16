173c1f92522198c708e0dda81029fab9fc555cb5
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-16 20:07:13 +0900

    [level 1] Title: 모의고사, Time: 3.65 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,21 +1,25 @@
 def solution(answers):
     answer = [0, 0, 0]
-    l = len(answers)
-    one = [1, 2, 3, 4, 5] * ((l-1)//5 + 1)
-    two = [2, 1, 2, 3, 2, 4, 2, 5] * ((l-1)//8 + 1)
-    thr = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * ((l-1)//10 + 1)
-
-    for a, b, c, ans in zip(one, two, thr, answers) :
-        if a == ans :
-            answer[0] += 1
-        if b == ans :
-            answer[1] += 1
-        if c == ans :
-            answer[2] += 1
+    ans = list()
+    one = [1, 2, 3, 4, 5]
+    two = [2, 1, 2, 3, 2, 4, 2, 5]
+    thr = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    winner = list()
-    max_n = max(answer)
+    l1 = len(one)
+    l2 = len(two)
+    l3 = len(thr)
+    
+    for i, n in enumerate(answers) :
+        if one[i%l1] == n :
+            answer[0] += 1
+        if two[i%l2] == n :
+            answer[1] += 1
+        if thr[i%l3] == n :
+            answer[2] += 1
+            
+    num = max(answer)
+    
     for i, n in enumerate(answer) :
-        if n == max_n :
-            winner.append(i+1)
-    return winner
+        if n == num :
+            ans.append(i+1)
+    return ans
