8d23aca47266a5b63814a1710b4a8dec64cfa004
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-16 19:55:25 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,13 +1,13 @@
 def solution(clothes):
     answer = 1
     clo_dict = dict()
-    
     for a, b in clothes :
         try :
             clo_dict[b] += 1
         except :
-            clo_dict[b] = 1
-            
-    for a, b in clo_dict.items() :
-        answer = answer * (b+1)
-    return answer -1
+            clo_dict[b] = 2
+    
+    for i in clo_dict.values() :
+        answer = answer*i
+    
+    return answer-1
