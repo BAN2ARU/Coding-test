215cd64d987267b7f225a0d07377e40057946ffb
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-17 20:40:18 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+c = int(input())
+for _ in range(c) :
+    score = list(map(int, input().split()))
+    avg = sum(score[1:]) / score[0]
+    cnt = 0
+    for s in score[1:] :
+        if s > avg :
+            cnt+=1
+    rate = cnt/score[0] * 100
+    print(f'{rate:.3f}%')
