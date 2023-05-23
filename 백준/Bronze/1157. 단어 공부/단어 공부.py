f62faf0bf935d1903bb3b20450edcfd1db043809
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-23 17:24:42 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,22 +1,15 @@
 import sys
 input = sys.stdin.readline
-word = input().rstrip().lower()
 
-word_dict = dict()
+word = input().rstrip().upper()
+uniq = list(set(word))
 
-for w in word :
-    try :
-        word_dict[w] += 1
-    except :
-        word_dict[w] = 1
+cnt = list()
+for s in uniq :
+    cnt.append(word.count(s))
 
-max_n = max(word_dict.values())
-cnt = 0
-answer = ''
-
-for s, n in word_dict.items() :
-    if n == max_n :
-        cnt+=1
-        answer=s
-
-print(answer.upper() if cnt==1 else '?')
+maximum = max(cnt)
+if cnt.count(maximum) > 1 :
+    print('?')
+else :
+    print(uniq[cnt.index(maximum)])
