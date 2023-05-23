82cce495fcae6f94567a8f4e7821700a7bf3407e
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-23 17:18:24 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,13 +1,12 @@
 import sys
-
 input = sys.stdin.readline
 
-alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+word = input().rstrip()
 answer = [-1] * len(alpha)
-word = input()
 
 for i, s in enumerate(alpha) :
     if s in word :
         answer[i] = word.index(s)
-
-print(*answer)        
+        
+print(*answer)
