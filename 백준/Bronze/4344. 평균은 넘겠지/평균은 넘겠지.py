65363071fe8e45aa314e8f7a39cc613736498201
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-24 12:41:05 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -2,12 +2,12 @@ import sys
 input = sys.stdin.readline
 
 c = int(input())
+
 for _ in range(c) :
     score = list(map(int, input().split()))
-    avg = sum(score[1:]) / score[0]
+    avg = sum(score[1:])/score[0]
     cnt = 0
-    for s in score[1:] :
-        if s > avg :
-            cnt+=1
-    rate = cnt/score[0] * 100
-    print(f'{rate:.3f}%')
+    for i in score[1:] :
+        if i > avg :
+            cnt += 1
+    print(f'{cnt/score[0]*100:.3f}%')
