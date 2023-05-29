f49c430313745e40ef92d1244ea1c654284aa208
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-29 14:38:42 +0900

    [level 2] Title: 구명보트, Time: 6.81 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,13 +1,14 @@
 def solution(people, limit):
+    answer = 0
     people.sort(reverse=True)
     l = len(people)
     b = l-1
-    a = 0
-    for i in range(l) :
-        if i >=b :
+    cnt = 0
+    for a in range(l) :
+        if a >= b :
             break
-        elif people[i]+people[b] <= limit :
+        elif people[a] + people[b] <= limit :
             b -= 1
-            a += 1
-    
-    return l-a
+            cnt += 1
+            
+    return l - cnt
