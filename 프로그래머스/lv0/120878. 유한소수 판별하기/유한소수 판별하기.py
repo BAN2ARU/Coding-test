abc801d6dca35a6bc69bf4e4d3cac5ea1e25a632
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-30 17:15:34 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,8 +1,8 @@
 def solution(a, b):
-    eq = lambda x,y : y if x%y == 0 else eq(y, x%y)
+    eq = lambda x,y : y if x%y==0 else eq(y, x%y)
     num = b // eq(a,b)
     while(num%2==0) :
-        num = num//2
+        num /= 2
     while(num%5==0) :
-        num = num//5
+        num /= 5
     return 1 if num == 1 else 2
