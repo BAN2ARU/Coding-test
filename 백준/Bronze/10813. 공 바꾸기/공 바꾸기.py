dcd52111a13ad348d56090ec5b8bc7d36d5c75b1
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-05-30 17:29:10 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,13 +1,12 @@
 import sys
-
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-answer = [i for i in range(1, n+1)]
+ball = list(range(1, n+1))
 
 for _ in range(m) :
     a, b = map(int, input().split())
-    answer[a-1], answer[b-1] = answer[b-1], answer[a-1]
+    ball[a-1], ball[b-1] = ball[b-1], ball[a-1]
+    
+print(*ball)
 
-for i in answer :
-    print(i, end=' ')
