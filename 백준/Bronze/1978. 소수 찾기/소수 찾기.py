f87babf277f3d1fb67c899bfc1ec246c117d5e13
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-06-04 21:56:02 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -6,10 +6,10 @@ num_list = list(map(int, input().split()))
 for num in num_list :
     if num == 1 :
         n -= 1
-    else :
-        for i in range(2, int(num**0.5)+1) :
-            if num%i==0 :
-                n -= 1
-                break
+        continue
+    for i in range(2, int(num**0.5)+1) :
+        if num%i==0 :
+            n -= 1
+            break
 
 print(n)
