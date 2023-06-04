64df89fc8a38b9b23f2c82577498e8bbc8cd6de2
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-06-04 21:55:05 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+num_list = list(map(int, input().split()))
+
+for num in num_list :
+    if num == 1 :
+        n -= 1
+    else :
+        for i in range(2, int(num**0.5)+1) :
+            if num%i==0 :
+                n -= 1
+                break
+
+print(n)
