20cd630bb9174b991290c2d6c64930663c315bc3
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-06-05 12:20:01 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,6 +1,3 @@
 def solution(rsp):
-    rsp_dict = {'2':'0', '0':'5', '5':'2'}
-    answer = ''
-    for r in rsp :
-        answer += rsp_dict[r]
-    return answer
+    rsp_dict = {'2':'0','0':'5','5':'2'}
+    return ''.join(rsp_dict[s] for s in rsp)
