4044c34d324f9dcd6b7944bdf96466de3b399dad
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-06-12 17:29:15 +0900

    [level 1] Title: 소수 찾기, Time: 361.52 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,7 +1,6 @@
 def solution(n):
-    num_set = set(range(2, n+1))
-    
-    for i in range(2, n+1) :
-        if i in num_set :
-            num_set -= set(range(i*2,n+1,i))
-    return len(num_set)
+    num = set(range(2, n+1))
+    for i in range(n+1) :
+        if i in num :
+            num -= set(range(i*2, n+1, i))
+    return len(num)
