83ba75c44e0b513348cf1bcd36393a767708d166
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-06-12 17:25:14 +0900

    [level 1] Title: 모의고사, Time: 8.88 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -2,21 +2,24 @@ def solution(answers):
     one = '12345'
     two = '21232425'
     thr = '3311224455'
-    l1, l2, l3 = len(one), len(two), len(thr)
-    ans = [0, 0, 0]
+    l_one = len(one)
+    l_two = len(two)
+    l_thr = len(thr)
+    answer = [0, 0, 0]
     
-    for i, n in enumerate(answers) :
-        if n == int(one[i%l1]) :
-            ans[0] += 1
-        if n == int(two[i%l2]) :
-            ans[1] += 1
-        if n == int(thr[i%l3]) :
-            ans[2] += 1
-    max_n = max(ans)
-    answer = list()
+    for i, ans in enumerate(answers) :
+        if ans == int(one[i%l_one]) :
+            answer[0] += 1
+        if ans == int(two[i%l_two]) :
+            answer[1] += 1
+        if ans == int(thr[i%l_thr]) :
+            answer[2] += 1
     
-    for i, n in enumerate(ans) :
-        if n == max_n :
-            answer.append(i+1)
+    correct = list()
+    max_n = max(answer)
     
-    return answer
+    for i, n in enumerate(answer) :
+        if max_n == n :
+            correct.append(i+1)
+    
+    return correct
