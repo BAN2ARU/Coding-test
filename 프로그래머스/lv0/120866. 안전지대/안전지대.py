d0075b363111f7b76401badfe28622a0685635d1
Na Rae Baek <93071842+BAN2ARU@users.noreply.github.com>
2023-06-19 15:52:07 +0900

    [level 0] Title: 안전지대, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -1,8 +1,8 @@
 def solution(board):
+    l = len(board)
     bomb = set()
     for i, n in enumerate(board) :
         for j, m in enumerate(board[i]) :
             if m :
-                bomb.update((i+a,j+b) for a in [-1, 0, 1] for b in [-1, 0, 1])
-    l = len(board)
-    return l*l - sum(0<=i<l and 0<=j<l for i,j in bomb)
+                bomb.update((i+a,j+b) for a in [-1,0,1] for b in [-1,0,1])
+    return l**2 - sum(0<=a<l and 0<=b<l for (a,b) in bomb)
